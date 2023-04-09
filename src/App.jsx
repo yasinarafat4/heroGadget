0249fa44218531778d3eb7ae4cc5d75215a987c8
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-04-09 20:49:38 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import React, { createContext, useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Outlet, useLoaderData } from "react-router-dom";
+import MyModal from "./components/Modal";
 
 export const ProductContext = createContext([]);
 export const CartContext = createContext([]);
 
 const App = () => {
+  //Modal
+  let [isOpen, setIsOpen] = useState(false);
+
   const { cartArray, products } = useLoaderData();
   const [cart, setCart] = useState(cartArray);
+
+  // show message when it user marked a product in past
+  const cartAlert = sessionStorage.getItem("alert");
+  if (cart.length > 0 && cartAlert !== "true") {
+    setIsOpen(true);
+    sessionStorage.setItem("alert", true);
+  }
+
   return (
     <ProductContext.Provider value={products}>
       <CartContext.Provider value={[cart, setCart]}>
@@ -17,6 +29,7 @@ const App = () => {
           <Outlet />
         </div>
         <Footer />
+        <MyModal isOpen={isOpen} setIsOpen={setIsOpen} />
       </CartContext.Provider>
     </ProductContext.Provider>
   );
